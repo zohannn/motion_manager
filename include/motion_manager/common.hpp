25dc39bde04c07455d2a134cb5442f338c463c74
Gianpaolo Gulletta <gianpaologulletta@gmail.com>
2017-04-07 21:53:47 +0100

    minor changes

--- include/motion_manager/common.hpp
+++ include/motion_manager/common.hpp
@@ -67,7 +67,7 @@ namespace motion_manager{
     // Barrett hand
     const double THETA8_HOME = 0.0; /**< constant of the joint 8 (spread of the hand) in [rad] */
     const double THETA8_FINAL = 0.0; /**< constant of the joint 8 (spread of the hand) in [rad] */
-    const double TOL_GRIP = 10.0; /**< tolerance on the grip in [mm] */
+    const double TOL_GRIP = 0.0; /**< tolerance on the grip in [mm] */
     const int firstPartTorqueOvershootCountRequired = 1;/**< number of time that the torque applied to the first phalanx is bigger than firstPartMaxTorque */
     const double firstPartMaxTorque = 0.9f;/**< max torque that can be applied to the first phalanx of the fingers */
     const double closingOpeningTorque = 1.0f;/**< torque applied to the fingers when they are opening/closing */
